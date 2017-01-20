fdac2917899c55411c6b5f1581be6ed30ca2e486
FaalFaazDov <biophasegames@gmail.com>
2017-01-20 11:09:52 -0800

    Create HexCell.cs

---
+++ Monobehaviours/HexCell.cs
@@ -0,0 +1,29 @@
+#region Header
+
+using UnityEngine;
+
+#endregion Header
+
+/**
+ * Component Model HexCell for use in HexGrid Methods.
+ * 
+ * Copyright (C) 2016  Copyright (C) 2016  Biophase Games
+ * 
+ * @category Biophase
+ * @package Biophase.HexGrid
+ * @subpackage HexCell
+ * @copyright Copyright (C) 2016  Biophase Games
+ * @license www.gnu.org/licenses/gpl.html
+ * @version 0.0.1Alpha
+ * @link https://github.com/BiophaseGames/HexGrid
+ * @since 2017-01-20
+ * 
+ */
+
+/**
+ * Monobehaviour Model HexCell for use in HexGrid Methods
+ */
+[AddComponentMenu("Biophase/Hex/Cell")]
+public class HexCell : MonoBehaviour
+{
+}
